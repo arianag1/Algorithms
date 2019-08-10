f9b0295b4bfb29d3db806f1a40e75561cff99c82
arianag1 <ariana.gardner0@gmail.com>
2019-08-10 00:55:54 -0400

    Update BalancedBrackets.java

--- Data-Structures/Stacks_Queues/BalancedBrackets.java
+++ Data-Structures/Stacks_Queues/BalancedBrackets.java
@@ -10,11 +10,24 @@ public class Solution {
 
     // Complete the isBalanced function below.
     static String isBalanced(String s) {
+        // check if the length of the string is odd, 
+        // if it is then brackets are not balanced. 
         if(s.length() % 2 != 0)return "NO";
 
         Stack<Character> openDelims = new Stack<Character>();
         Stack<Character> closingDelims = new Stack<Character>();
-
+        
+        // Search through the string and check if it starts with an
+        // opening delimiter. If it does then push the char on the 
+        // open delimiter stack. 
+        
+        // If the string starts with a closing delimiter
+        // (open delimiter stack is empty) then is it not balanced. 
+        // Else check if 
+            // open delimiter contains { and the current character in the string is }
+            // or open delimiter contains ( and the current character in the string is )
+            // or open delimiter contains [ and the current character in the string is ]
+            // otherwise add the current character to the closing delimiter stack.  
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
             if(ch == '{' || ch == '(' || ch == '[')
@@ -32,7 +45,12 @@ public class Solution {
                     closingDelims.push(ch);
             }
         }
-
+        
+        // Need to make sure that both stacks are empty, 
+        // if they are then return YES. Otherwise try
+        // to match opening delimiter with closing delimiter
+        // from the top of each stack. If this doesn't work
+        // return NO. 
         while(!openDelims.empty() || !closingDelims.empty()){
             if(!openDelims.empty() && closingDelims.empty())
                 return "NO";
